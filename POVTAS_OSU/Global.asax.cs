c1b4ef5f53e7b9be0d1999a939bdd5fd88c265dd
Mila18 <milana.ab@mail.ru>
2021-10-20 18:36:56 +0500

    Миграция с Bootstrap 3 на Bootstrap 5.

--- POVTAS_OSU/Global.asax.cs
+++ POVTAS_OSU/Global.asax.cs
@@ -16,6 +16,7 @@ namespace POVTAS_OSU
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
